f864963d9ad6b6640676545696bcc151f78037f6
iosdev.sw <iosdev.sw@gmail.com>
2023-06-25 21:22:41 +0900

    Initial Settings

---
+++ FitHub/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
+
---
+++ FitHub/Views/TempView.swift
@@ -0,0 +1,8 @@
+//
+//  TempView.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/06/25.
+//
+
+import Foundation
