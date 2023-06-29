f19d6d75ba208a700fec5e70eebe1745e32c2a76
iosdev.sw <iosdev.sw@gmail.com>
2023-06-29 18:49:20 +0900

    Feat: 커스텀 통신사 선택 ActionSheet 추가

---
+++ FitHub/ViewModels/Auth/RegistInfoViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  RegistInfoViewModel.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/06/29.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class RegistInfoViewModel {
+    let telecomProviders = Observable.of(TelecomProviderType.allCases)
+    
+    let selectedTelecomProvider: BehaviorRelay<TelecomProviderType?> = BehaviorRelay(value: nil)
+    
+    init() {
+        print(TelecomProviderType.allCases)
+    }
+    
+}
