a4f6d564ffe2cf14ef475c88d2f0514bc656a482
iosdev.sw <iosdev.sw@gmail.com>
2023-07-03 18:25:38 +0900

    Feat: 커스텀 얼럿 컴포넌트 구현

---
+++ FitHub/Utils/Extension/NotificationName+.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/07/03.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let dismissStandardAlert = Notification.Name("dismissStandardAlert")
+}
