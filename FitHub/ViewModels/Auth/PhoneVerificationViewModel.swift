35ac093dee65e430d1892e79e41ddd4032596a19
iosdev.sw <iosdev.sw@gmail.com>
2023-07-05 02:16:31 +0900

    Feat: 비밀번호 설정UI 구현

---
+++ FitHub/ViewModels/Auth/PhoneVerificationViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  PhoneVerificationViewModel.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/07/04.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class PhoneVerificationViewModel: ViewModelType {
+    var disposeBag = DisposeBag()
+
+    let userInfo: BehaviorRelay<RegistUserInfo>
+    struct Input {
+        let authenticationNumber: Observable<String>
+        let resendTap: Signal<Void>
+        let nextTap: Signal<Void>
+    }
+
+    struct Output {
+        let nextButtonEnable: Observable<Bool>
+        let authNumber: Observable<String>
+        let resendTap: Signal<Void>
+        let nextTap: Signal<Void>
+    }
+
+    init(userInfo: BehaviorRelay<RegistUserInfo>) {
+        self.userInfo = userInfo
+    }
+
+    func transform(input: Input) -> Output {
+        let nextButtonEnable = input.authenticationNumber
+            .map { String($0.prefix(6)).count == 6 }
+
+        let authNumber = input.authenticationNumber
+            .distinctUntilChanged()
+            .map { String($0.prefix(6)) }
+        
+        return Output(nextButtonEnable: nextButtonEnable,
+                      authNumber: authNumber,
+                      resendTap: input.resendTap,
+                      nextTap: input.nextTap
+        )
+    }
+}
