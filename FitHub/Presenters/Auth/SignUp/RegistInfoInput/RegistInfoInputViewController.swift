d8b2ff205a0b1cc96c142458c52e873d2bf7f06d
iosdev.sw <iosdev.sw@gmail.com>
2023-07-11 17:25:05 +0900

    Edit: StatusImage 마진 수정

--- FitHub/Presenters/Auth/SignUp/RegistInfoInput/RegistInfoInputViewController.swift
+++ FitHub/Presenters/Auth/SignUp/RegistInfoInput/RegistInfoInputViewController.swift
@@ -15,7 +15,7 @@ final class RegistInfoInputViewController: BaseViewController {
     private let viewModel: RegistInfoViewModel
     
     private let titleLabel = UILabel().then {
-        $0.text = "휴대폰 번호로 가입"
+        $0.text = "휴대폰번호로 가입"
         $0.font = .pretendard(.headLineSmall)
         $0.textColor = .textDefault
     }
@@ -26,7 +26,7 @@ final class RegistInfoInputViewController: BaseViewController {
         $0.textColor = .textSub02
     }
     
-    private let phoneNumberInputTextFieldView = StandardTextFieldView("휴대폰 번호").then {
+    private let phoneNumberInputTextFieldView = StandardTextFieldView("휴대폰번호").then {
         $0.placeholder = "01012345678"
         $0.textField.keyboardType = .numberPad
     }
