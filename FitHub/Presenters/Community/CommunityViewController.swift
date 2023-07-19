636a5a639e95b5433a61288b9049b413a7cb0449
iosdev.sw <iosdev.sw@gmail.com>
2023-07-20 01:00:34 +0900

    Chore: 커뮤니티 이미지리소스 추가

--- FitHub/Presenters/Community/CommunityViewController.swift
+++ FitHub/Presenters/Community/CommunityViewController.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 final class CommunityViewController: BaseViewController {
+    let searchBar = UISearchBar().then {
+        
+        $0.searchTextField.layer.masksToBounds = true
+        $0.searchTextField.backgroundColor = .bgSub01
+        $0.searchTextField.layer.cornerRadius = 20
+    }
+    
     override func configureUI() {
         self.navigationItem.leftBarButtonItem = nil
     }
+    
+    override func configureNavigation() {
+        let noti = UIBarButtonItem(image: UIImage(named: "BackButton"), style: .plain, target: nil, action: nil)
+        let bookmark = UIBarButtonItem(image: UIImage(named: "BackButton"), style: .plain, target: nil, action: nil)
+        
+        self.navigationItem.rightBarButtonItems = [bookmark,noti]
+        
+        self.navigationItem.titleView = searchBar
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
+
