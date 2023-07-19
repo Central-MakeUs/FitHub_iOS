083bf3f0c963cc11a5cd76de5b307b93dd3073ae
iosdev.sw <iosdev.sw@gmail.com>
2023-07-19 16:46:45 +0900

    Feat: 탭바컨트롤러 추가

---
+++ FitHub/Presenters/Community/CommunityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CommunityViewController.swift
+//  FitHub
+//
+//  Created by 신상우 on 2023/07/19.
+//
+
+import UIKit
+
+final class CommunityViewController: BaseViewController {
+    override func configureUI() {
+        self.navigationItem.leftBarButtonItem = nil
+    }
+}
