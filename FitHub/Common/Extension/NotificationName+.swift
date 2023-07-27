2091efbdb2457e2530a78aa2933ec4daaedfc5c7
iosdev.sw <iosdev.sw@gmail.com>
2023-07-27 22:37:47 +0900

    Feat: 로그인화면 띄우는 noti 추가

--- FitHub/Common/Extension/NotificationName+.swift
+++ FitHub/Common/Extension/NotificationName+.swift
@@ -9,4 +9,5 @@ import Foundation
 
 extension Notification.Name {
     static let dismissStandardAlert = Notification.Name("dismissStandardAlert")
+    static let presentAlert = Notification.Name("presentAlert")
 }
